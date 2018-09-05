a954bba309fb20b479fa6622a6b420674616fd39
Greg <tubatica@gmail.com>
2018-09-05 13:40:18 -0600

    initial

---
+++ src/components/main.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Main = () => (
+  <div className="container">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Memory Game
+      </a>
+    <div>Score</div>
+    </nav>
+    <div className="jumbotron">
+      <h1>Your Project</h1>
+      <p>
+        Enim adipisicing enim reprehenderit ex ullamco consectetur Lorem laboris magna exercitation.
+        Aliquip reprehenderit magna elit cillum adipisicing dolore aliquip velit. Ipsum ullamco
+        nostrud tempor eu deserunt ipsum incididunt fugiat esse ipsum. Exercitation nostrud
+        exercitation sit ex nostrud aliqua officia magna nostrud deserunt et esse eu deserunt. Non
+        dolor consequat qui ea culpa tempor magna nulla consectetur est occaecat.
+      </p>
+    </div>
+    
+  </div>
+);
+
+export default Main;
